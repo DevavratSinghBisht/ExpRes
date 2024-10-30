add50f4f37d6596cbadebab4bfb4bf296599a900
DevavratSinghBisht <devavratsinghbisht@gmail.com>
2024-10-30 04:24:28 -0700

    added doc strings

--- backend/utils/cryptography.py
+++ backend/utils/cryptography.py
@@ -1,8 +1,16 @@
 import hashlib
 
-def getEncoding(data:str, hasher:str = "sha256"):
+def getEncoding(data:str, hashAlg:str = "sha256") -> str:
+    '''
+    Encodes string cryptographically
+
+    data    : string to be encoded
+    hashAlg : hash algorithm to ber used
+
+    return  : hashed string 
+    '''
 
     data_bytes = data.encode('utf-8')
 
-    if hasher == "sha256":
+    if hashAlg == "sha256":
         return hashlib.sha256(data_bytes).hexdigest()
--- backend/utils/html.py
+++ backend/utils/html.py
@@ -1,3 +1,4 @@
+# Will be removed lateron
 chatHTML = """
 <!DOCTYPE html>
 <html>
